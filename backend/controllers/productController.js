bb05460f83306a3acae5bcc577f242f440534119
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-10-29 13:44:15 +0545

    getProducts, getProductById controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,14 @@
 import asynchandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
+/***************************************
+ * @desc Fetch all products
+ * @route GET /api/products
+ * @access Public
+ * @returns {object} - products
+ *
+***************************************/
+
 export const getProducts = asynchandler(async (req, res) => {
   const products = await Product.find({});
 
@@ -12,7 +20,16 @@ export const getProducts = asynchandler(async (req, res) => {
   res.status(200).json({ products });
 });
 
-export const getSingleProduct = asynchandler(async (req, res) => {
+/**
+ * @desc Fetch single product
+ * @route GET /api/products/:id
+ * @access Public
+ * @returns {object} - product
+ *
+ */
+
+
+export const getProductById = asynchandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
   if (product) {
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
 import Product from "../models/productModel.js";
-import { getProducts, getSingleProduct } from "../controllers/productController.js";
+import { getProductById, getProducts } from "../controllers/productController.js";
 
 router.route("/").get(getProducts);
-router.route("/:id").get(getSingleProduct)
+router.route("/:id").get(getProductById)
 export default router;
