6c605f3c78000ddb0e196d0778bdbbb18d9782c6
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-11-01 12:09:48 +0545

    orderController -updateOrderToPaid

--- backend/config/index.js
+++ backend/config/index.js
@@ -5,6 +5,8 @@ const CONFIG = {
     MONGO_URI: process.env.MONGO_URI,
     NODE_ENV: process.env.NODE_ENV,
     JWT_SECRET: process.env.JWT_SECRET,
+    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
+    PAYPAL_CLIENT_SECRET: process.env.PAYPAL_CLIENT_SECRET,
 }
 
 export default CONFIG
--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -82,13 +82,32 @@ export const getOrderById = asynchandler(async (req, res) => {
 
 /***************************************
  * @desc Update order to paid
- * @route GET /api/orders/:id/pay
+ * @route PUT /api/orders/:id/pay
  * @access Private
  * @returns {object} - Order
  * ***************************************/
 
 export const updateOrderToPaid = asynchandler(async (req, res) => {
-  res.send("Update order to paid");
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+
+  order.isPaid = true;
+  order.paidAt = Date.now();
+  // From paypal
+  order.paymentResult = {
+    id: req.body.id,
+    status: req.body.status,
+    update_time: req.body.update_time,
+    email_address: req.body.payer.email_address,
+  };
+
+  const updatedOrder = await order.save();
+
+  res.status(200).json(updatedOrder);
 });
 
 /***************************************
