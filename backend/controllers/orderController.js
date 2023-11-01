e599b8dad33c442cc6537e8d459af0089261b8a5
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-11-01 08:23:46 +0545

    order -get, create, getById controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,114 @@
+import asynchandler from "../middleware/asyncHandler.js";
+import Order from "../models/orderMode.js";
+
+/***************************************
+ * @desc Create new Order
+ * @route POST /api/orders
+ * @access Private
+ * @returns {object} - Orders
+ *
+ ***************************************/
+
+export const addOrderItems = asynchandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    itemsPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  }
+
+  const order = new Order({
+    user: req.user._id,
+    orderItems: orderItems.map((x) => ({
+      ...x,
+      product: x._id,
+      _id: undefined,
+    })),
+    shippingAddress,
+    paymentMethod,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    itemsPrice,
+  });
+
+  const createdOrder = await order.save();
+
+  res.status(201).json(createdOrder);
+});
+
+/***************************************
+ * @desc Get logged in user orders
+ * @route GET /api/myorders
+ * @access Private
+ * @returns {object} - Order
+ *
+ ***************************************/
+
+export const getMyOrders = asynchandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
+});
+
+/***************************************
+ * @desc Get order by id
+ * @route GET /api/orders/:id
+ * @access Private
+ * @returns {object} - Order
+ *
+ ***************************************/
+
+export const getOrderById = asynchandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+
+  res.status(200).json(order);
+});
+
+/***************************************
+ * @desc Update order to paid
+ * @route GET /api/orders/:id/pay
+ * @access Private
+ * @returns {object} - Order
+ * ***************************************/
+
+export const updateOrderToPaid = asynchandler(async (req, res) => {
+  res.send("Update order to paid");
+});
+
+/***************************************
+ * @desc Update to delivered
+ * @route GET /api/orders/:id/deliver
+ * @access Private/Admin
+ * @returns {object} - Order
+ * ***************************************/
+
+export const updateOrderToDelivered = asynchandler(async (req, res) => {
+  res.send("Update order to delivered");
+});
+
+/***************************************
+ * @desc Get all orders
+ * @route GET /api/orders
+ * @access Private/Admin
+ * @returns {object} - Order
+ * ***************************************/
+
+export const getOrders = asynchandler(async (req, res) => {
+  res.send("Get all orders  ");
+});
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import CONFIG from "./config/index.js";
 import connectDB from "./db/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
@@ -29,6 +30,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
